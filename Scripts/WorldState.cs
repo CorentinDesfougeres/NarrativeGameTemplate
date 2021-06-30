e861ac818846d7e4fe6642a9e26ec21508e76595
CorentinDesfougeres <72070778+CorentinDesfougeres@users.noreply.github.com>
2021-07-01 00:01:38 +0200

    General Folders Organisation

---
+++ Scripts/WorldState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tetri
+{
+	
+	[CreateAssetMenu(menuName = "Tetri/Sally/World State", fileName = "NewWorldState")]
+	public class WorldState : ScriptableObject
+	{
+        
+		
+
+    }
+	
+}
